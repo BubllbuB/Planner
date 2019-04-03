3647f6ae2b96d4877c0119cbd290f66eb935fac7
BubllbuB <stepanfoweb@gmail.com>
2019-04-03 19:06:24 +0300

    TabAdapter

---
+++ app/src/main/java/com/example/planner/adapter/TabAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.planner.adapter
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+
+class TabAdapter(fragmentManager: FragmentManager): FragmentStatePagerAdapter(fragmentManager) {
+    private val mFragmentList = arrayListOf<Fragment>()
+    private val mFragmentTitleList = arrayListOf<String>()
+
+    override fun getItem(position: Int): Fragment {
+        return mFragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        return mFragmentList.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mFragmentTitleList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        mFragmentList.add(fragment)
+        mFragmentTitleList.add(title)
+    }
+}
--- app/src/main/java/com/example/planner/fragments/MainContentFragment.kt
+++ app/src/main/java/com/example/planner/fragments/MainContentFragment.kt
@@ -1,78 +1,52 @@
 package com.example.planner.fragments
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TabHost
 import com.example.planner.FRAGMENT_TAG_ADD_TASK
 import com.example.planner.R
 import com.example.planner.enums.TaskKey
 import kotlinx.android.synthetic.main.fragment_main.view.*
+import com.example.planner.adapter.TabAdapter
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
 
-const val TAB_ALL = "tabAll"
-const val TAB_FAV = "tabFavorite"
+
 
 class MainContentFragment : Fragment() {
+    private lateinit var viewPager: ViewPager
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
 
         init(view)
 
-        requireActivity().supportFragmentManager.beginTransaction()
-            .add(R.id.tab1, AllTasksFragment())
-            .commit()
-
-        requireActivity().supportFragmentManager.beginTransaction()
-            .add(R.id.tab2, FavoritesTasksFragment())
-            .commit()
-
         return view
     }
 
     private fun init(view: View) {
-        val tabHost = view.findViewById<TabHost>(R.id.tabHost)
-        tabHost.setup()
+        viewPager = view.findViewById(R.id.viewPager)
+        tabLayout = view.findViewById(R.id.tabLayout)
 
-        this.setNewTab(
-            requireContext(),
-            tabHost,
-            TAB_ALL,
-            R.string.textTabAllTasks,
-            android.R.drawable.star_on,
-            R.id.tab1
-        )
-        this.setNewTab(
-            requireContext(),
-            tabHost,
-            TAB_FAV,
-            R.string.textTabFavoriteTasks,
-            android.R.drawable.star_on,
-            R.id.tab2
-        )
+        val tabAdapter = TabAdapter(requireFragmentManager())
+        tabAdapter.addFragment(AllTasksFragment(), getString(R.string.textTabAllTasks))
+        tabAdapter.addFragment(FavoritesTasksFragment(), getString(R.string.textTabFavoriteTasks))
 
+        viewPager.adapter = tabAdapter
+        tabLayout.setupWithViewPager(viewPager)
 
         view.fab.setOnClickListener {
             val fragment = AddTaskFragment()
             val bundle = Bundle()
-            bundle.putBoolean(TaskKey.KEY_TASK_FAV.getKey(), tabHost.currentTabTag == TAB_FAV)
+            //bundle.putBoolean(TaskKey.KEY_TASK_FAV.getKey(), tabHost.currentTabTag == TAB_FAV)
             fragment.arguments = bundle
             requireActivity().supportFragmentManager.beginTransaction()
                 .replace(R.id.content_fragments, fragment, FRAGMENT_TAG_ADD_TASK)
                 .addToBackStack(null)
                 .commit()
         }
-    }
-
-    private fun setNewTab(context: Context, tabHost: TabHost, tag: String, title: Int, icon: Int, contentID: Int) {
-        val tabSpec = tabHost.newTabSpec(tag)
-        tabSpec.apply {
-            setContent(contentID)
-            setIndicator(getString(title), context.resources.getDrawable(icon, null))
-        }
-        tabHost.addTab(tabSpec)
     }
 }
